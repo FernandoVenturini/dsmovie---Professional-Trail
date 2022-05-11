160643eb7f89ac1e06d79106ab2734bc23a7f713
Fernando Venturini <fernandoventurini9@hotmail.com>
2022-05-11 22:04:27 +0100

    Project created

---
+++ backend/src/main/java/com/fernandoventurini/dsmovie/Trilha/Profissional/DsmovieTrilhaProfissionalApplication.java
@@ -0,0 +1,13 @@
+package com.fernandoventurini.dsmovie.Trilha.Profissional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmovieTrilhaProfissionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmovieTrilhaProfissionalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fernandoventurini/dsmovie/Trilha/Profissional/DsmovieTrilhaProfissionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandoventurini.dsmovie.Trilha.Profissional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieTrilhaProfissionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
