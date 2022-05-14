3151cf8c459e24d22410a2e3ffa9e31aa083c1f3
Fernando Venturini <fernandoventurini9@hotmail.com>
2022-05-14 01:44:56 +0100

    Find movies

--- backend/src/main/java/com/fernandoventurini/dsmovie/Trilha/Profissional/controllers/MovieController.java
+++ backend/src/main/java/com/fernandoventurini/dsmovie/Trilha/Profissional/controllers/MovieController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
